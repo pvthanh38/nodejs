4bb69b6e6daf06fb9d6d1669bec357d19a533015
pvthanh38 <pvthanh38@gmail.com>
2015-11-05 07:04:36 +0700

    the3

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
-//var server = http.createServer();
+var io = require('socket.io')(http);
 var fs = require('fs');
 var data = "";
 var port = 3000;
-var host = 'http://localhost'; 
-function handel(req, res){ 
-res.writeHead(200,{'Content-Type':'text-plain'});
-res.write('Hello world');
-res.end();
-}
+var host = 'http://192.168.1.106'; 
+
 app.get('/',function(req, res){
+	res.sendFile('index.html');
+	/*
 	fs.appendFile('readme.txt','Here me', function(error){
 		if(error) throw error;
 		console.log("The file was save!");
@@ -22,9 +20,22 @@ app.get('/',function(req, res){
 		}else{
 			res.writeHead(200,{'Content-type':'text/html'});
 			res.end(data);
-	}});
+	}});*/
 	
 });
+io.on('connection',function(socket){
+	socket.on('adduser', function(username){
+		socket.username = username;
+		console.log(socket.username);
+		
+	});
+	socket.on('send_mgs', function(user, msg){
+		io.emit('send_mgs',socket.username,user, msg);
+		console.log(user);
+	});
+	
+});
+
 
 //server.on('request', readfile);
 http.listen(port);
